60ebf20d13714a26516bf38a03d269dc4de11075
loteque <andrewandroid@gmail.com>
2024-06-12 08:33:29 -0700

    integrate run y anims, closes #23

--- AnimationController.gd
+++ AnimationController.gd
@@ -4,14 +4,26 @@ extends Node
 @export var sprite: AnimatedSprite2D
 
 func _physics_process(_delta):
+    if !actor:
+        return
+    
     if actor.velocity != Vector2.ZERO:
         if actor.velocity.x < 0:
-            sprite.play("run_side_0")
-            actor.global_transform.x.x = -1
+            if actor.velocity.y > 0 and abs(actor.velocity.x) < abs(actor.velocity.y):
+                sprite.play("run_down")
+            else:
+                sprite.play("run_side_0")
+                actor.global_transform.x.x = -1
     
         if actor.velocity.x >= 0:
-            sprite.play("run_side_0")
-            actor.global_transform.x.x = 1
-    
+            if actor.velocity.y < 0 and abs(actor.velocity.x) < abs(actor.velocity.y):
+               sprite.play("run_up") 
+            elif actor.velocity.y > 0 and abs(actor.velocity.x) < abs(actor.velocity.y):
+                sprite.play("run_down")
+            else: 
+                sprite.play("run_side_0")
+                actor.global_transform.x.x = 1
+
+        
     else:
         sprite.play("idle_front")
