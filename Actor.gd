6cc158f564ffcbe3494d9c4948284dbeb1c6695f
loteque <andrewandroid@gmail.com>
2024-06-12 19:11:31 -0700

    integrate thrown anims, closes #24

--- Actor.gd
+++ Actor.gd
@@ -8,29 +8,52 @@ extends CharacterBody2D
 var has_target: bool = false
 var target: Node2D
 var throw_target: Marker2D
+var player: Node2D
+var player_inside_follow_area: bool = false
 
+var _is_thrown: bool = false
+func is_thrown():
+    if _is_thrown:
+        return true
+    return false
 
 func _ready() -> void:
     nav_agent.velocity_computed.connect(Callable(_on_velocity_computed))
+    nav_agent.target_reached.connect(_on_target_reached)
 
-func set_movement_target(movement_target: Vector2):
+func _set_movement_target(movement_target: Vector2):
     nav_agent.set_target_position(movement_target)
 
 func _physics_process(_delta):
-    
-    if has_target or throw_target:
- 
-        set_movement_target(target.global_transform.origin)
-    
+
     if nav_agent.is_navigation_finished(): 
-        if !follow_area.monitoring:
-            follow_area.monitoring = true
-            set_collision_layer(1)
-            set_collision_mask(1)
-            target = self
         
+        if (
+            player_inside_follow_area 
+            and self.global_transform.origin.distance_to(
+                    player.global_transform.origin
+                ) < nav_agent.target_desired_distance
+        ):
+            velocity = Vector2.ZERO
+            return
+
+        if (
+            player_inside_follow_area 
+            and self.global_transform.origin.distance_to(
+                    player.global_transform.origin
+                ) > nav_agent.target_desired_distance
+        ):
+            target = player
+            has_target = true
+            _set_movement_target(target.global_transform.origin)
+            
         velocity = Vector2.ZERO
         return
+
+    if has_target or throw_target:
+        _set_movement_target(target.global_transform.origin)
+    
+
 
     var next_path_position: Vector2 = nav_agent.get_next_path_position()
     var new_velocity: Vector2 = global_position.direction_to(next_path_position) * move_speed
@@ -39,13 +62,13 @@ func _physics_process(_delta):
 
     _on_velocity_computed(new_velocity)
 
-
 func _input(event):
     if event.is_action("throw") and has_target:
-        follow_area.monitoring = false
         set_collision_layer(0)
         set_collision_mask(0)
         target = throw_target
+        _is_thrown = true
+        _set_movement_target(target.global_transform.origin)
 
 func _on_velocity_computed(safe_velocity: Vector2):
     velocity = safe_velocity
@@ -56,12 +79,24 @@ func _on_follow_area_body_entered(body:Node2D):
     
     if body.is_in_group("Player"):
         has_target = true
-        target = body
+        player = body
+        target = player
+        player_inside_follow_area = true
         throw_target = body.throw_target
-
+        _set_movement_target(body.global_transform.origin)
 
 func _on_follow_area_body_exited(body:Node2D):
+    
     if body.is_in_group("Player"):
         has_target = false
         target = self
+        player_inside_follow_area = false
         print("body exited")
+
+func _on_target_reached():
+    _is_thrown = false
+    follow_area.monitoring = true
+    set_collision_layer(1)
+    set_collision_mask(1)
+    target = self
+    
--- AnimationController.gd
+++ AnimationController.gd
@@ -3,27 +3,52 @@ extends Node
 @export var actor: CharacterBody2D
 @export var sprite: AnimatedSprite2D
 
+func play_thrown_animation(rotation_deg: float):
+    sprite.play("thrown_down")
+    sprite.rotation_degrees = rotation_deg
+
 func _physics_process(_delta):
+    
     if !actor:
         return
     
-    if actor.velocity != Vector2.ZERO:
-        if actor.velocity.x < 0:
-            if actor.velocity.y > 0 and abs(actor.velocity.x) < abs(actor.velocity.y):
-                sprite.play("run_down")
-            else:
-                sprite.play("run_side_0")
-                actor.global_transform.x.x = -1
-    
-        if actor.velocity.x >= 0:
-            if actor.velocity.y < 0 and abs(actor.velocity.x) < abs(actor.velocity.y):
-               sprite.play("run_up") 
-            elif actor.velocity.y > 0 and abs(actor.velocity.x) < abs(actor.velocity.y):
-                sprite.play("run_down")
-            else: 
-                sprite.play("run_side_0")
-                actor.global_transform.x.x = 1
+    sprite.rotation = 0
 
-        
-    else:
+    if actor.velocity == Vector2.ZERO:
         sprite.play("idle_front")
+        return
+
+    if actor.velocity.x < 0:
+        if actor.velocity.y > 0 and abs(actor.velocity.x) < abs(actor.velocity.y):
+            if actor.is_in_group("throwable") and actor.is_thrown():
+                play_thrown_animation(180)
+                return
+            else:
+                sprite.play("run_down")
+        else:
+            if actor.is_in_group("throwable") and actor.is_thrown():
+                play_thrown_animation(180)
+                return
+            sprite.play("run_side_0")
+            actor.global_transform.x.x = -1
+
+    if actor.velocity.x >= 0:
+        
+        if actor.velocity.y > 0 and abs(actor.velocity.x) < abs(actor.velocity.y):
+            sprite.play("run_down")
+        
+        if actor.velocity.y < 0 and abs(actor.velocity.x) <= abs(actor.velocity.y):
+            if actor.is_in_group("throwable") and actor.is_thrown():
+                play_thrown_animation(180)
+                return
+            else:
+                sprite.play("run_up") 
+        
+        else:
+            if actor.is_in_group("throwable") and actor.is_thrown():
+                play_thrown_animation(180)
+                return
+            sprite.play("run_side_0")
+            actor.global_transform.x.x = 1
+
+    
