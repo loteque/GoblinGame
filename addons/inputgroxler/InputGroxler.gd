c8a3993d745b9346b047c3c1d189f031c24134b2
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2024-06-14 11:14:48 -0700

    Remove input addon

--- addons/inputgroxler/InputGroxler.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends EditorPlugin
-
-func _enter_tree():
-	add_custom_type("ControllerManager", "Node", preload("src/ControllerManager.gd"), preload("icon.png"))
-
-func _exit_tree():
-	remove_custom_type("ControllerManager")
