2d192c6ec0f95d2959b4898be3e5511d6a361d7e
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2024-06-19 20:49:47 -0700

    Add hurt animation

---
+++ project/src/character/hurt_state.gd
@@ -0,0 +1,24 @@
+extends State
+
+## Createure is "in_combat"
+class_name HurtState
+
+@onready var actor_core = %ActorCore
+var animated_sprite: AnimatedSprite2D:
+    get: return actor_core.animated_sprite
+
+@onready var hurt_timer = $HurtTimer
+
+func _on_hurt_timeout():
+    actor_core.is_hurt = false
+
+func _ready():
+    hurt_timer.timeout.connect(_on_hurt_timeout)
+
+func enter_state(data: Dictionary = {}):
+    super.enter_state()
+    animated_sprite.play("hurt")
+    hurt_timer.start()
+
+func exit_state():
+    super.exit_state()
