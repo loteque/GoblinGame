318f34841e47ab86cb3af7da42bab7d4f454eeb4
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2024-06-19 19:58:10 -0700

    Adds health bar UI

---
+++ project/src/ui/health_bar.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@export var health_component : HealthComponent
+
+@onready var progress_bar = $ProgressBar
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    progress_bar.value = health_component.current_health
+    progress_bar.max_value = health_component.max_health
+    health_component.health_changed.connect(_on_health_changed)
+
+func _on_health_changed(new_health: int):
+    progress_bar.value = new_health
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    pass
