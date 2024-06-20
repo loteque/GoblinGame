b727b8aec5a99132148aa77a44ac213023bc34eb
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2024-06-19 20:16:26 -0700

    Adds health bars
    Closes #86

--- project/src/ui/health_bar.gd
+++ project/src/ui/health_bar.gd
@@ -9,9 +9,14 @@ func _ready():
     progress_bar.value = health_component.current_health
     progress_bar.max_value = health_component.max_health
     health_component.health_changed.connect(_on_health_changed)
+    if health_component.current_health == health_component.max_health:
+        progress_bar.visible = false
 
 func _on_health_changed(new_health: int):
     progress_bar.value = new_health
+    if not health_component.current_health == health_component.max_health:
+        progress_bar.visible = true
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
     pass
