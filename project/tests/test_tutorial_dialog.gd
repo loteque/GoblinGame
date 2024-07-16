86e79e20982397a5cbeda3da5ae63d4f6e6a49e3
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2024-07-16 01:29:34 -0700

    Add dialog start

---
+++ project/tests/test_tutorial_dialog.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	start_dialog()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func start_dialog():
+	Dialogic.timeline_ended.connect(_on_timeline_ended)
+	Dialogic.start("01-intro")
+
+func _on_timeline_ended():
+	Dialogic.timeline_ended.disconnect(_on_timeline_ended)
+	# do something else here
